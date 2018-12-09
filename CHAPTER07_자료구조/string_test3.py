218fd5ef44743cb61b85ead99db8fae362ff5d8b
LeeEunHak <x771200@naver.com>
2018-12-09 11:16:18 +0900

    string_test3

---
+++ CHAPTER07_자료구조/string_test3.py
@@ -0,0 +1,13 @@
+# 문자열 변환하기
+
+s = input("문자열을 입력하세요: ")
+
+print(s.upper())        # 모든 문자를 대문자로
+print(s.lower())        # 모든 문자를 소문자로
+print(s.capitalize())   # 첫 번째 문자만을 대문자로
+
+# 예시
+# 문자열 입력: my name is leeeunhak.
+# MY NAME IS LEEEUNHAK.
+# my name is leeeunhak.
+# My name is leeeunhak.
